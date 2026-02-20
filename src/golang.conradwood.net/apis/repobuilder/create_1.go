c0831569ca36ff8cf4dbaf21162eed652f15cb62
Conrad Wood <cnw@conradwood.net>
2026-02-20 14:42:30 +0000

    automatic commit
    Change-Id: I8e33c9203cee8c701ccbdcc00c95b2f4ae99c1b1

--- src/golang.conradwood.net/apis/repobuilder/create_1.go
+++ src/golang.conradwood.net/apis/repobuilder/create_1.go
@@ -4,7 +4,7 @@
 */
 
 /* geninfo:
-   filename  : protos/golang.conradwood.net/apis/repobuilder/repobuilder.proto
+   filename  : golang.conradwood.net/apis/repobuilder/repobuilder.proto
    gopackage : golang.conradwood.net/apis/repobuilder
    importname: ai_0
    clientfunc: GetRepoBuilder
